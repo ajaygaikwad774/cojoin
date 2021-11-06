95e16b0583d07d4f37ccc3da29de3439aff5b734
ajaygaikwad774 <ajay.gaikwad@LPT2444.newgen.co.in>
2021-11-06 16:13:47 +0530

    Initial Commit

---
+++ peg/src/main/java/com/smn/el/PegApplication.java
@@ -0,0 +1,13 @@
+package com.smn.el;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegApplication.class, args);
+	}
+
+}
---
+++ peg/src/test/java/com/smn/el/PegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smn.el;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
